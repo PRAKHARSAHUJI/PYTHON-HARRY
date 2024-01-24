d39fc63e90535caf53d02bcd0ddd66ecbd363469
PRAKHARSAHUJI <prakharsahu@Prakhars-MacBook-Air.local>
2024-01-24 11:31:50 +0530

    add

--- Chapter 5/02_dictionary_methods.py
+++ Chapter 5/02_dictionary_methods.py
@@ -8,11 +8,24 @@ dictionary = {
 print("\n",dictionary.keys(),"\n")
 print(list(dictionary.keys()),"\n")
 
+
+print(type(dictionary.keys()))
+
+
 print("\n",dictionary.values(),"\n")
 print(list(dictionary.values()),"\n")
 
-print(dictionary.items(),"\n")
+
+print(type(dictionary.values()))
+
+
+print(dictionary.items(),"\n")          # Print all the contents of the dictionary  
 print(list(dictionary.items()))
+
+
+print(type(dictionary.items()))
+
+
 
 newDict = { 
     "Hobby" : " Football and Table Tennis",
@@ -23,8 +36,11 @@ dictionary.update(newDict)
 print(dictionary,"\n")
 
 
+
 print(dictionary.get("Name")) # This will not throw error if it is not present . show none if not present 
 print(dictionary["Name"]) # This will  throw error if it is not present . show error
 
 
 # MORE methods are present in docs.python.org
+
+print(dictionary.update(newDict))
--- Chapter 5/04_sets_methods.py
+++ Chapter 5/04_sets_methods.py
@@ -9,9 +9,44 @@ b = {1,2,4,5,3,6}
 # SET METHODS
 
 # 01 : string.add
-
+a.add(5)
+a.add(6)
+a.add(9)
 a.add(1)
 a.add(2)
 a.add(1)
 
 print(a)
+# we can add tuple in set as that is hashable , it cant be changed
+# we cant add list because it is unhashable , it can be changed
+# we cant add dictionary because it is unhashable , it can be changed
+
+a.add((1,2,3))  # tuple
+
+
+
+# a.union({2,3,4})
+print(a.union({2,3,4}))
+
+print(a.intersection({2,3,4,5,6}))
+
+
+
+
+
+print(len(a))
+print(a)
+
+a.remove(1)
+print(a)
+
+
+print(a.pop()) # print the element that has been removed by the pop command  
+
+print(a)
+
+
+print(a.clear())
+print(a)
+
+
